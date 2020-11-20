4c3a03e47ee08a05bf44038a1586d1eb90fd8dea
Jude <storeyjude@hotmail.com>
2020-11-20 14:01:27 +0000

    deployment

--- foyle_valley_farms/settings.py
+++ foyle_valley_farms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'o14(w1i=#yb3m%il+xq)23o9picei-h-eo#osy&yex8vpqq$v7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['foyle-valley-farms.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,19 @@ WSGI_APPLICATION = 'foyle_valley_farms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
