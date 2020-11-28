96f0fac857455f058cab2e0032954dec6c7793d0
Jude <storeyjude@hotmail.com>
2020-11-28 16:28:59 +0000

    updated blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from .models import Post, Comment
+
+
+
 #https://djangocentral.com/building-a-blog-application-with-django/
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status','created_on')
-    list_filter = ("status",)
+    list_filter = ("status", 'created_on')
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
   
-admin.site.register(Post, PostAdmin)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
@@ -19,3 +22,5 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(active=True)
+
+admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -29,6 +30,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+
+        return reverse("post_detail", kwargs={"slug": str(self.slug)})
 
 # Adding comments to blog posts https://djangocentral.com/creating-comments-system-with-django/
 
