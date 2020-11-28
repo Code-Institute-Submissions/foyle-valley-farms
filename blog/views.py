e3b980f1bbf3c2c1b3b9f68b2a572f488c2be3b0
Jude <storeyjude@hotmail.com>
2020-11-28 12:35:40 +0000

    blog app articles section created

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
 
 # Create your views here.
 # https://djangocentral.com/adding-pagination-with-django/
 
 
-def blog(request):
-    
-    return render(request, 'blog/blog.html')
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog/blog.html'
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
